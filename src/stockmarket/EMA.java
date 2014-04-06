1fda9324c5d4d9321b1397089d9eafb5f92e20d5
Saurabh2993 <saurabhghadi@gmail.com>
2014-04-06 23:00:00 +0530

    try to solve the null pointer exception in EMA

--- src/stockmarket/EMA.java
+++ src/stockmarket/EMA.java
@@ -6,6 +6,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -18,6 +19,8 @@ public class EMA {
     static final String PASS = "Saurabh";
     static Connection conn = null;
     static Statement stmt = null;
+    String D[];
+    String C[];
     
     public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException, ParseException {
     	   
@@ -31,7 +34,7 @@ public class EMA {
         String query = "SELECT Symbol FROM test.companylist";
         stmt = conn.createStatement();
         ResultSet rs = stmt.executeQuery(query);
-        while (rs.next() && i<2) {
+        while (rs.next() && i<5) {
             String test=rs.getString("Symbol");
             System.out.println(test);
             i++;
@@ -43,37 +46,33 @@ public class EMA {
     
     public void run(String Symbol) throws FileNotFoundException, IOException, SQLException, ParseException {
     	
-    	SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MMM-dd");
+    	DateFormat df = new SimpleDateFormat("yyyy-MMM-dd");
+    	Date date;
         String csvFile = "E:\\Users\\Saurabh\\Documents\\GitHub\\stockmarket\\csv files\\"+Symbol+".csv";
         BufferedReader br = null;
         String line = "";
         String split = ",";
-        
-        String query="Drop table "+Symbol; 
-        stmt = conn.createStatement();
-        stmt.executeUpdate(query);
-        
-        String query1="CREATE table "+Symbol+" (Date DATE,Open DOUBLE,High DOUBLE,Low DOUBLE,Close DOUBLE,Volume DOUBLE)"; 
-        stmt = conn.createStatement();
-        stmt.executeUpdate(query1);
-        
+
         try {
             
-            br = new BufferedReader(new FileReader(csvFile));
+        	br = new BufferedReader(new FileReader(csvFile));
             System.out.println("Connected database successfully...");
-            while((line = br.readLine()) != null) {
+            for(int i=0;(line = br.readLine()) != null;i++) {
                 
                 String[] data = line.split(split);
-                Date date = formatter.parse(data[0]);
+                //date = df.parse(data[0]);
+                //String newDateString = df.format(date);
+                //System.out.println(newDateString);
+                //D[i]=newDateString;
+                //System.out.println(D[i]);
                 String Open=data[1];
                 String High=data[2];
                 String Low=data[3];
                 String Close=data[4];
+                System.out.println(Close);
+                C[i]=Close;
+                System.out.println(C[i]);
                 String Volume=data[5];
-                
-                String query2 = "Insert into "+Symbol+" values('"+date+"','"+Open+"','"+High+"','"+Low+"','"+Close+"','"+Volume+"',PRIMARY KEY("+date+")";
-                stmt = conn.createStatement();
-                stmt.executeUpdate(query2);
             }
             
             
